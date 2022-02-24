080602706292bfdc1809a15c057043c2c491c7df
Ahmed Saad <ahmed.saad.kader@gmail.com>
2022-02-24 14:46:53 +0200

    setup server

--- server.js
+++ server.js
@@ -2,18 +2,26 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require('express');
 // Start up an instance of app
-
+const app = express();
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
-
 // Cors for cross origin allowance
-
+const cors = require('cors');
+app.use(cors());
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static('./website'))
 
-
+const port = 8000;
 // Setup Server
+const server = app.listen(port, ()=>{console.log(`running on localhost:${port}`)})
+// Initialize all route with a callback function
+app.get('/all', GetCallBack)
+// Callback function to complete GET '/all'
+function GetCallBack (req,res){
+    res.send(projectData)
+}
